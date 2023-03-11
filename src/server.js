1e447848eefe08e0ee57d606da66f30c508a1c08
Thiago Santos <thiago.santos@vr.com.br>
2023-03-11 10:51:54 -0300

    feat: Criando duas rotas (GET/POST) com o servidor no modo stateful.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,24 @@
 import http from 'node:http';
 
+const users = [];
+
 const server = http.createServer((req, res) => {
+    const { method, url } = req;
+
+    if(method === 'GET' && url === '/users') {
+        return res.setHeader('Content-type', 'application/json').end(JSON.stringify(users));
+    }
+
+    if(method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@mail.com',
+        });
+
+        return res.end('Usuário criado com sucesso!');
+    }
+
     return res.end('Hello World')
 });
 
